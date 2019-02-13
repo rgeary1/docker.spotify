9f2caa2f5979b982ad7f81ba774faee0ed28bec0
Richard Geary <richard@skylake-capital.com>
2019-02-12 19:33:52 -0500

    Update to add now-required ffmpeg dependency, support multiple host users
    Change-Id: I41af991ab83e14535f55c1a5c825b5ce244d6c72

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x 
-if ! docker volume inspect spotify >/dev/null 2>/dev/null; then
-  docker volume create spotify
+if ! docker volume inspect spotify.$USER >/dev/null 2>/dev/null; then
+  docker volume create spotify.$USER
 fi
-docker build -t spotify --build-arg USER=$USER --build-arg UID=$UID .
+docker build -t spotify.$USER --build-arg USER=$USER --build-arg UID=$UID .
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -d -e DISPLAY --device /dev/snd -v /tmp/.X11-unix:/tmp/.X11-unix -v spotify:/home/$USER spotify
+docker run --rm -d -e DISPLAY --device /dev/snd -v /tmp/.X11-unix:/tmp/.X11-unix -v spotify.$USER:/home/$USER --hostname=desktop spotify.$USER
